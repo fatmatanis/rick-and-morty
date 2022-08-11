e1973502dc36bd9d3a2842a6eac09843864c2083
Fatma Tanış <97517723+fatmatanis@users.noreply.github.com>
2022-08-11 20:18:36 +0300

    pages deleted

--- src/pages/EpisodeCharacters/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const EpisodeCharacters = () => {};
-
-export default EpisodeCharacters;
--- src/pages/Favorites/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-const Favorites = () => {};
-
-export default Favorites;
