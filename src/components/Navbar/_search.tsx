feba3a6e35a187c76753b87c3361915488993580
Fatma Tanış <97517723+fatmatanis@users.noreply.github.com>
2022-08-15 15:36:05 +0300

    Update src/components/Navbar/_search.tsx
    Co-authored-by: Abdullah Tanrıverdi <89466000+loneddonkey@users.noreply.github.com>

--- src/components/Navbar/_search.tsx
+++ src/components/Navbar/_search.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { INavbarProps } from "../../types/types";
 
-const Search = ({ styleName }: INavbarProps) => {
-  return <input type="search" placeholder="Search" className={styleName} />;
+const Search = ({ className }: INavbarProps) => {
+  return <input type="search" placeholder="Search" className={className} />;
 };
 
 export default Search;
