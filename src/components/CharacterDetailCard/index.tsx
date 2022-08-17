3dbddb6e6b584415de61975fba5757dec1eeac0d
Fatma Tanış <97517723+fatmatanis@users.noreply.github.com>
2022-08-17 12:03:57 +0300

    character detail card added

---
+++ src/components/CharacterDetailCard/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CharacterDetailCard = () => {
+  return (
+    <div className="character-detail">
+      <div className="character-detail-title">Status</div>
+      <div className="character-detail-text">Alive</div>
+    </div>
+  );
+};
+
+export default CharacterDetailCard;
