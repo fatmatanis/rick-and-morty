068be069c276bc7c2979498b6fdb979f5adc9be4
Fatma Tanış <97517723+fatmatanis@users.noreply.github.com>
2022-08-17 22:05:27 +0300

    title count props added

--- src/components/TitleCount/index.tsx
+++ src/components/TitleCount/index.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import rightArrow from "../../assets/rightArrow.svg";
+import { ITitleCountProps } from "../../types/types";
 
-const TitleCount = () => {
+const TitleCount = ({ link, text, count }: ITitleCountProps) => {
   return (
-    <Link to="/" className="title-count-link">
+    <Link to={link} className="title-count-link">
       <div className="title-count-container">
-        <div className="title-count-text">Character</div>
-        <div className="title-count-number">43</div>
+        <div className="title-count-text">{text}</div>
+        <div className="title-count-number">{count}</div>
         <img className="right-arrow" src={rightArrow} alt="right-arrow" />
       </div>
     </Link>
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,9 @@
 export interface INavbarProps {
   className: string;
 }
+
+export interface ITitleCountProps {
+  link: string;
+  text: string;
+  count: number;
+}
