a32ef1e397bc86a76ce2160c19a82c1186bcb5c9
Fatma Tanış <97517723+fatmatanis@users.noreply.github.com>
2022-08-25 15:28:08 +0300

    types added

--- src/types/types.ts
+++ src/types/types.ts
@@ -29,12 +29,44 @@ export interface ICharacterCardProps {
   id: number;
   image: string;
   name: string;
-  origin: string;
+  origin?: string;
   species: string;
 }
 
 export interface INavigationProps {
   colorStyles: string;
+}
+
+export interface ICharacterListProps {
+  characters: ICharacter[];
+  cardCount: number;
+}
+
+export interface ICharacter {
+  id: number;
+  image: string;
+  name: string;
+  type: string;
+  status: string;
+  gender: string;
+  species: string;
+  location?: ILocation;
+  origin: ILocation;
+  episode?: IEpisode[];
+}
+
+export interface ILocation {
+  id?: number;
+  name?: string;
+  residents?: ICharacter[];
+  dimension?: string;
+  type?: string;
+}
+export interface IEpisode {
+  id: number;
+  name: string;
+  episode: string;
+  characters: ICharacter[];
 }
 
 interface ISelectedFilter {
