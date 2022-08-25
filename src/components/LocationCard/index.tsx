ab12feb913e3b54216795b6637b7c015a03aace3
Fatma Tanış <97517723+fatmatanis@users.noreply.github.com>
2022-08-25 18:29:08 +0300

    location card created

---
+++ src/components/LocationCard/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LocationCard = () => {
+  return (
+    <div className="location-card-wrapper">
+      <div>
+        <span>Earth (C-137)</span>
+      </div>
+    </div>
+  );
+};
+
+export default LocationCard;
